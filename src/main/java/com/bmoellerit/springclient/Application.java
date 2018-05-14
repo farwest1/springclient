baa23d3a7601e7fabc65fa674f9d6d07379b6dec
Bernd <moeller-bernd@gmx.de>
2018-05-14 09:10:18 +0200

    Added Zuul Proxy

--- src/main/java/com/bmoellerit/springclient/Application.java
+++ src/main/java/com/bmoellerit/springclient/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 @EnableAutoConfiguration
 public class Application {
 
