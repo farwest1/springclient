aec64a8cbc868c3ffa3bee2dd604b753262234fe
Bernd <moeller-bernd@gmx.de>
2018-05-14 10:09:46 +0200

    Enhanced zuul configuration

--- src/main/java/com/bmoellerit/springclient/api/HelloController.java
+++ src/main/java/com/bmoellerit/springclient/api/HelloController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpStatus;
@@ -29,8 +30,8 @@ public class HelloController {
   @Autowired
   private DiscoveryClient discoveryClient;
 
-
-  private String appName = "springdemo";
+  @Value("${springclient.servicename}")
+  private String appName;
 
 
 
